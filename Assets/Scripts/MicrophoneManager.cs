5cfc09d452ddbe425196525e0692d17cd781ba62
大塚 正弥 <masaya.otsuka@jbs.com>
2018-05-09 14:34:34 +0900

    New create repository

---
+++ Assets/Scripts/MicrophoneManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+
+
+public class MicrophoneManager : MonoBehaviour
+{
+    public static MicrophoneManager instance; //help to access instance of this object
+    private int frequency = 44100;      //recording frequency of mic
+    private AudioSource audioSource;        //AudioSource component, provides access to mic
+    private bool microphoneDetected;        //flag indicating mic detection
+    private bool isCapturingAudio;      //flag indicating audio capture status
+    private DictationRecognizer dictationRecognizer;  //Component converting speech to text
+    
+
+    private void Awake()
+    {
+        // Set this class to behave similar to singleton
+        instance = this;
+    }
+
+    void Start()
+    {
+        //Use Unity Microphone class to detect devices and setup Audiosource
+        if (Microphone.devices.Length > 0)
+        {
+            Results.instance.SetMicrophoneStatus("Initialising...");
+            audioSource = GetComponent<AudioSource>();
+            microphoneDetected = true;
+        }
+        else
+        {
+            Results.instance.SetMicrophoneStatus("No Microphone detected");
+        }
+    }
+
+    /// <summary>
+    /// Start microphone capture. Debugging message is delivered to the Results class.
+    /// </summary>
+    public void StartCapturingAudio()
+    {
+        if (microphoneDetected)
+        {
+            // Start microphone capture
+            isCapturingAudio = true;
+
+            // Start dictation
+            dictationRecognizer = new DictationRecognizer();
+            dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+            dictationRecognizer.Start();
+
+            // Update UI with mic status
+            Results.instance.SetMicrophoneStatus("Capturing...");
+        }
+    }
+
+    /// <summary>
+    /// Stop microphone capture. Debugging message is delivered to the Results class.
+    /// </summary>
+    public void StopCapturingAudio()
+    {
+        Results.instance.SetMicrophoneStatus("Mic sleeping");
+        isCapturingAudio = false;
+        Microphone.End(null);
+        dictationRecognizer.DictationResult -= DictationRecognizer_DictationResult;
+        dictationRecognizer.Dispose();
+    }
+
+
+    /// <summary>
+    /// Start microphone capture. Debugging message is delivered to the Results class.
+    /// </summary>
+    //public void StartCapturingAudio()
+    //{
+    //    if (microphoneDetected)
+    //    {
+    //        // Start microphone capture
+    //        isCapturingAudio = true;
+
+    //        // Start dictation
+    //        dictationRecognizer = new DictationRecognizer();
+    //        dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+    //        dictationRecognizer.Start();
+
+    //        // Update UI with mic status
+    //        Results.instance.SetMicrophoneStatus("Capturing...");
+    //    }
+    //}
+
+    /// <summary>
+    /// Stop microphone capture. Debugging message is delivered to the Results class.
+    /// </summary>
+
+
+    //public void StopCapturingAudio()
+    //{
+    //    Results.instance.SetMicrophoneStatus("Mic sleeping");
+    //    isCapturingAudio = false;
+    //    Microphone.End(null);
+    //    dictationRecognizer.DictationResult -= DictationRecognizer_DictationResult;
+    //    dictationRecognizer.Dispose();
+    //}
+
+    /// <summary>
+    /// This handler is called every time the Dictation detects a pause in the speech. 
+    /// Debugging message is delivered to the Results class.
+    /// </summary>
+    private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
+    {
+        // Update UI with dictation captured
+        Results.instance.SetDictationResult(text);
+
+        // Start the coroutine that process the dictation through Azure 
+        StartCoroutine(Translator.instance.TranslateWithUnityNetworking(text));
+    }
+
+}
---
+++ Assets/Scripts/Translator.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Runtime.Serialization;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Translator : MonoBehaviour {
+
+public static Translator instance;
+
+private string translationTokenEndpoint = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+private string translationTextEndpoint = "https://api.microsofttranslator.com/v2/http.svc/Translate?";
+
+private const string ocpApimSubscriptionKeyHeader = "Ocp-Apim-Subscription-Key";
+
+//Substitute the value of authorizationKey with your own Key
+private const string authorizationKey = "cb6f35c426954555924c562e807f752a";
+
+private string authorizationToken;
+
+// languages set below are:
+// English
+// French
+// Italian
+// Japanese
+// Korean
+public enum Languages { en, fr, it, ja, ko };
+public Languages from = Languages.en;
+public Languages to = Languages.it;
+
+private void Awake()
+{
+    // Set this class to behave similar to singleton
+    instance = this;
+}
+
+// Use this for initialization
+void Start()
+{
+    // When the application starts, request an auth token 
+    StartCoroutine("GetTokenCoroutine", authorizationKey);
+}
+
+/// <summary>
+/// Request a Token from Azure Translation Service by providing the access key.
+/// Debugging result is delivered to the Results class.
+/// </summary>
+private IEnumerator GetTokenCoroutine(string key)
+{
+    if (string.IsNullOrEmpty(key))
+    {
+        throw new InvalidOperationException("Authorization key not set.");
+    }
+
+    WWWForm webForm = new WWWForm();
+
+    using (UnityWebRequest unityWebRequest =
+    UnityWebRequest.Post(translationTokenEndpoint, webForm))
+    {
+        unityWebRequest.SetRequestHeader("Ocp-Apim-Subscription-Key", key);
+
+        // The download handler is responsible for bringing back the token after the request
+        unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
+
+        yield return unityWebRequest.SendWebRequest();
+
+        authorizationToken = unityWebRequest.downloadHandler.text;
+
+        if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+        {
+            Results.instance.azureResponseText.text = unityWebRequest.error;
+        }
+
+        long responseCode = unityWebRequest.responseCode;
+
+        // Update the UI with the response code
+        Results.instance.SetAzureResponse(responseCode.ToString());
+    }
+
+    // After receiving the token, begin capturing Audio with the MicrophoneManager Class
+    MicrophoneManager.instance.StartCapturingAudio();
+    StopCoroutine("GetTokenCoroutine");
+    yield return null;
+}
+
+/// <summary>
+/// Request a translation from Azure Translation Service by providing a string. 
+/// Debugging result is delivered to the Results class.
+/// </summary>
+public IEnumerator TranslateWithUnityNetworking(string text)
+{
+    WWWForm webForm = new WWWForm();
+    string result;
+    string queryString;
+
+    // This query string will contain the parameters for the translation
+    queryString = string.Concat("text=", Uri.EscapeDataString(text), "&from=", from, "&to=", to);
+
+    using (UnityWebRequest unityWebRequest = UnityWebRequest.Get(translationTextEndpoint + queryString))
+    {
+        unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
+        unityWebRequest.SetRequestHeader("Authorization", "Bearer " + authorizationToken);
+        unityWebRequest.SetRequestHeader("Accept", "application/xml");
+
+        yield return unityWebRequest.SendWebRequest();
+
+        string deliveredString = unityWebRequest.downloadHandler.text;
+
+        // The response will be in Json format
+        // Therefore we need to deserialise it
+        DataContractSerializer serializer;
+        serializer = new DataContractSerializer(typeof(string));
+
+        using (Stream stream = GenerateStreamFromString(deliveredString))
+        {
+            // Set the UI with the translation
+            Results.instance.SetTranslatedResult((string)serializer.ReadObject(stream));
+        }
+
+        if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+        {
+            Debug.Log(unityWebRequest.error);
+        }
+
+
+        StopCoroutine("TranslateWithUnityNetworking");
+
+    }
+}
+
+public static Stream GenerateStreamFromString(string incomingString)
+{
+    MemoryStream stream = new MemoryStream();
+    StreamWriter writer = new StreamWriter(stream);
+    writer.Write(incomingString);
+    writer.Flush();
+    stream.Position = 0;
+    return stream;
+}
+
+}
